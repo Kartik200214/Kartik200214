933315830fa55e662a625603acb9cfc0d990dd31
Kartik -KK <109861395+Kartik200214@users.noreply.github.com>
2022-09-09 17:56:53 +0530

    Add files via upload

---
+++ else.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a ,b;
+    cout<<"Enter two numbers"<<endl;
+    cin>> a;
+    cin >>b;
+
+    
+    if (a==b){
+        cout<<"Hey these are equal"<<endl;
+    }
+else{
+        if(a < b){
+            cout<<"a is smaller"<<endl;
+        }else{
+            cout<<"a is greater"<<endl;
+        }
+
+        
+    }
+}
